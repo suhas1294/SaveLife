844a630c073e183ac0910ee10968f6e68d0722f2
ananthmysore <ananthj87@gmail.com>
2015-12-19 16:21:19 +0530

    Initial UI

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,19 @@
+class StaticController < ActionController::Base
+  layout "static"
+
+def index
+end
+
+def item_1
+end
+
+def item_2
+end
+
+def item_3
+end
+
+def item_4
+end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,10 @@ Rails.application.routes.draw do
       get 'info/server' => 'info#server'
     end
   end
+  #static pages
+  get 'static/index' => 'static#index'
+  get 'static/item-1' => 'static#item_1'
+  get 'static/item-2' => 'static#item_2'
+  get 'static/item-3' => 'static#item_3'
+  get 'static/item-4' => 'static#item_4'
 end
