dd819b7c295de56a07c39310bc6934ead137676b
Raghavendra Joshi <rjoshi@qwinixtech.com>
2015-12-19 16:37:12 +0530

    - static files are converted to erb files

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,24 @@
 Rails.application.routes.draw do
+
   root 'welcome#index'
-  get 'home_page'      => 'welcome#index'
+
   # For API's
   namespace :api do
     namespace :v1 do
       get 'info/server' => 'info#server'
     end
   end
+
   #static pages
   get 'static/index' => 'static#index'
   get 'static/item-1' => 'static#item_1'
   get 'static/item-2' => 'static#item_2'
   get 'static/item-3' => 'static#item_3'
   get 'static/item-4' => 'static#item_4'
+
+  #welcome pages
+  get 'welcome/item-1' => 'welcome#item_1'
+  get 'welcome/item-2' => 'welcome#item_2'
+  get 'welcome/item-3' => 'welcome#item_3'
+  get 'welcome/item-4' => 'welcome#item_4'
 end
