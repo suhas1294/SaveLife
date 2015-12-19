d0309efe8e4a1116c2ae6d51c81b1a578100e57c
Raghavendra Joshi <rjoshi@qwinixtech.com>
2015-12-19 19:02:05 +0530

    - web regstration contoller

--- app/controllers/web/registrations_controller.rb
+++ app/controllers/web/registrations_controller.rb
@@ -5,6 +5,20 @@ class Web::RegistrationsController < ApplicationController
   end
 
   def create
-
+    user = User::Creation.new(user_params).create
+    if user.errors.blank?
+      # mail sending code
+      # redirect
+    else
+      render 'new'
+    end
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :device_token, :device_id, :address =>[:address_1, :address_2, :city, :state_id, :country_id], :profile => [:first_name, :last_name, :blood_group_id, :mobile_number, :other_number]
+  )
+  end
+
 end
